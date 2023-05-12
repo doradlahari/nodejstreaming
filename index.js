4254c610bca8250598fe21fa887a47d92997814a
Doradla Hari <doradla.hari@gmail.com>
2023-05-12 12:29:31 +0530

    pushed with text nature and category belongs to particular text

--- index.js
+++ index.js
@@ -121,73 +121,104 @@ app.get("/", (req, res) => {
 //   res.json({ past: `I ${past}`, present: `I ${present}`, future: `I will ${future}` });
 // });\
 
+// const natural = require('natural');
+// const MongoClient = require('mongodb').MongoClient;
+// // const db = client.db(dbName);
+// // const collection = db.collection(collectionNamesecond);
+// // MongoDB connection URL
+// const url = "mongodb+srv://doradlahari:nlp@cluster0.0eg4uqv.mongodb.net/" ;
+
+// // MongoDB database name
+// const dbNamesecond = 'mydatabase';
+
+// // MongoDB collection name for training data
+// const collectionNamesecond = 'trainingData';
+
+// // Define training data
+// const trainingData = [
+//   { text: 'I love Node.js', category: 'technology' },
+//   { text: 'I hate spiders', category: 'animals' },
+//   { text: 'I enjoy playing guitar', category: 'music' },
+//   { text: 'I am afraid of heights', category: 'fears' },
+//   { text: 'I love hiking in the mountains', category: 'nature' },
+// ];
+
+
+// // Connect to MongoDB and insert training data
+// // MongoClient.connect(url,   dbNamesecond +
+// //       "?retryWrites=true&w=majority",
+// //     { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+// //   if (err) {
+// //     console.error('Error connecting to MongoDB', err);
+// //     return;
+// //   }
+
+
+//   // Insert training data into collection
+//   // collection.insertMany(trainingData, (err, result) => {
+//   //   if (err) {
+//   //     console.error('Error inserting training data into MongoDB', err);
+//   //     client.close();
+//   //     return;
+//   //   }
+
+//     // console.log('Training data inserted into MongoDB:', result.insertedCount);
+
+//     // Create classifier and train it with the training data
+//     const classifier = new natural.BayesClassifier();
+//     // collection.find().toArray((err, documents) => {
+//     //   if (err) {
+//     //     console.error('Error retrieving training data from MongoDB', err);
+//     //     client.close();
+//     //     return;
+//     //   }
+
+//       // documents.forEach(document => {
+//       //   classifier.addDocument(document.text, document.category);
+//       // });
+
+//       classifier.train();
+
+//       // Define API endpoint for text classification
+//       app.post('/classify', (req, res) => {
+//         const textToClassify = req.body.text;
+//         const category = classifier.classify(textToClassify);
+//         res.json({ category });
+//       });
+
+//       console.log('Classifier trained with training data from MongoDB');
+//       // client.close();
+
+
+
+// text classifications using node js check in post man
+const bodyParser = require('body-parser');
 const natural = require('natural');
-const MongoClient = require('mongodb').MongoClient;
-const db = client.db(dbName);
-const collection = db.collection(collectionNamesecond);
-// MongoDB connection URL
-const url = "mongodb+srv://doradlahari:nlp@cluster0.0eg4uqv.mongodb.net/" ;
-
-// MongoDB database name
-const dbNamesecond = 'mydatabase';
-
-// MongoDB collection name for training data
-const collectionNamesecond = 'trainingData';
-
-// Define training data
+const classifier = new natural.BayesClassifier();
 const trainingData = [
-  { text: 'I love Node.js', category: 'technology' },
-  { text: 'I hate spiders', category: 'animals' },
-  { text: 'I enjoy playing guitar', category: 'music' },
-  { text: 'I am afraid of heights', category: 'fears' },
-  { text: 'I love hiking in the mountains', category: 'nature' },
+  { text: 'I love coding', category: 'technology' },
+  { text: 'I hate coding', category: 'technology' },
+  { text: 'I love playing music', category: 'music' },
+  { text: 'I hate playing music', category: 'music' },
+  { text: 'I love cooking', category: 'food' },
+  { text: 'I hate cooking', category: 'food' }
 ];
 
+trainingData.forEach(item => classifier.addDocument(item.text, item.category));
 
-// Connect to MongoDB and insert training data
-MongoClient.connect(url,   dbNamesecond +
-      "?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-  if (err) {
-    console.error('Error connecting to MongoDB', err);
-    return;
+classifier.train();
+
+app.use(bodyParser.json());
+
+app.post('/classify', (req, res) => {
+  const text = req.body.text;
+
+  if (!text) {
+    return res.status(400).json({ error: 'Missing text parameter' });
   }
 
+  const category = classifier.classify(text);
 
-  // Insert training data into collection
-  collection.insertMany(trainingData, (err, result) => {
-    if (err) {
-      console.error('Error inserting training data into MongoDB', err);
-      client.close();
-      return;
-    }
-
-    console.log('Training data inserted into MongoDB:', result.insertedCount);
-
-    // Create classifier and train it with the training data
-    const classifier = new natural.BayesClassifier();
-    collection.find().toArray((err, documents) => {
-      if (err) {
-        console.error('Error retrieving training data from MongoDB', err);
-        client.close();
-        return;
-      }
-
-      documents.forEach(document => {
-        classifier.addDocument(document.text, document.category);
-      });
-
-      classifier.train();
-
-      // Define API endpoint for text classification
-      app.post('/classify', (req, res) => {
-        const textToClassify = req.body.text;
-        const category = classifier.classify(textToClassify);
-        res.json({ category });
-      });
-
-      console.log('Classifier trained with training data from MongoDB');
-      client.close();
-    });
-  });
+  res.json({ category });
 });
+
